7ed06e40e2535083332f465d4d9e84865525965d
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-10-04 15:40:10 -0500

    added project card outline

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
   return (
     <div>
       <Header />
       <h1>Benjamin Vasko</h1>
+      <ProjectCard />
       <Footer />
     </div>
   );
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Button from "react-bootstrap/Button";
+
+function ProjectCard() {
+    return (
+
+<CardGroup>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+      <Button variant="primary">Start</Button>
+      <Button variant="primary">GitHub Repository</Button>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+      <Button variant="primary">Start</Button>
+      <Button variant="primary">GitHub Repository</Button>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+
+</CardGroup>
+
+    );
+}
+
+export default ProjectCard;
