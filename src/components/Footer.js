d4b98a521d60f16ed5b3490045133de273000bfb
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-10-10 11:20:21 -0500

    added font awesome icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,12 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-
-const styles = {
-
-    center: {
-        margin: "auto",
-        fontSize: "12px",
-        fontStyle: "italic"
-    }
-
-};
+import "./Footer.css";
 
 function Footer() {
     return (
 
-            <Navbar bg="primary" variant="dark" sticky="bottom">
-                <Navbar.Brand style={styles.center}>Finis origine pendet.</Navbar.Brand>
+            <Navbar className="footer" bg="primary" variant="dark" sticky="bottom">
+                <p className="text">Finis origine pendet.</p>
             </Navbar>
 
     );
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,46 +2,114 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import "./ProjectCard.css";
 
 function ProjectCard() {
-    return (
+  return (
+    <div>
 
-<CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+      <CardGroup>
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Spark Forums</Card.Title>
+            <Card.Text>
+              A forum to discuss what's on your mind free of censorship.
       </Card.Text>
-      <Button variant="primary">Start</Button>
-      <Button variant="primary">GitHub Repository</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
+            <Button className="buttonStart" variant="primary"><FontAwesomeIcon className="icon fa-lg" icon={faPlay} />Start</Button>
+            <Button className="buttonGithub" variant="primary"><FontAwesomeIcon className="icon fa-lg" icon={faGithub} />
+            <span className="githubText">Github Repository</span>
+            </Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 9/19/20</small>
+          </Card.Footer>
+        </Card>
 
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Wheels Up</Card.Title>
+            <Card.Text>
+              A travel app which makes it easy to track where you've been and where you're going.
       </Card.Text>
-      <Button variant="primary">Start</Button>
-      <Button variant="primary">GitHub Repository</Button>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
+            <Button className="buttonStart" variant="primary">Start</Button>
+            <Button variant="primary">GitHub Repository</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 8/20/20</small>
+          </Card.Footer>
+        </Card>
 
-</CardGroup>
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Spark Forums</Card.Title>
+            <Card.Text>
+              A forum to discuss what's on your mind free of censorship.
+      </Card.Text>
+            <Button className="buttonStart" variant="primary">Start</Button>
+            <Button variant="primary">GitHub Repository</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 9/19/20</small>
+          </Card.Footer>
+        </Card>
 
-    );
+      </CardGroup>
+
+      <CardGroup>
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Spark Forums</Card.Title>
+            <Card.Text>
+              A forum to discuss what's on your mind free of censorship.
+      </Card.Text>
+            <Button className="buttonStart" variant="primary">Start</Button>
+            <Button variant="primary">GitHub Repository</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 9/19/20</small>
+          </Card.Footer>
+        </Card>
+
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Wheels Up</Card.Title>
+            <Card.Text>
+              A travel app which makes it easy to track where you've been and where you're going.
+      </Card.Text>
+            <Button className="buttonStart" variant="primary">Start</Button>
+            <Button variant="primary">GitHub Repository</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 8/20/20</small>
+          </Card.Footer>
+        </Card>
+
+        <Card className="projectCard">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Spark Forums</Card.Title>
+            <Card.Text>
+              A forum to discuss what's on your mind free of censorship.
+      </Card.Text>
+            <Button className="buttonStart" variant="primary">Start</Button>
+            <Button variant="primary">GitHub Repository</Button>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 9/19/20</small>
+          </Card.Footer>
+        </Card>
+
+      </CardGroup>
+    </div>
+  );
 }
 
 export default ProjectCard;
