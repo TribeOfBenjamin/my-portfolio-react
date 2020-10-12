0bb319dd4c85c7b21bc2889b6a410a610e896068
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-10-11 21:38:44 -0500

    Added react-router and paths to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
-import ProjectCards from "./components/ProjectCards/ProjectCards";
-import Header from "./components/Header/Header";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <ProjectCards />
-    </div>
+    <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/Portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectCards from "../ProjectCards/ProjectCards";
+import Header from "../Header/Header";
+
+function Portfolio() {
+  return (
+      <div>
+        <Header />
+        <ProjectCards />
+      </div>
+  );
+}
+
+export default Portfolio;
