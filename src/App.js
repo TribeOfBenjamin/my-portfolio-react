ab02e28272e2adb2727a2a6265fa099822f7b791
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-10-11 22:23:46 -0500

    styled homepage: buttons, inline, floating effect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "../Header/Header";
+import "./Home.css";
 
 function Home() {
-  return (
-      <div>
-        <Header />
-      </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <h2>Hello there!</h2>
+            </div>
+            <div className="container">
+                <p>I'm Ben. and I'm <button className="buttonTeacher">a teacher</button> & <button className="buttonWebDev">a web developer</button> .</p>
+                <br />
+                <p className="floating">Learn more by clicking above</p>
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
