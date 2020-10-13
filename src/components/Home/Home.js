ab77a8b1f24d540d7c08fcf4bcaa9e717174a99d
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-10-12 22:04:59 -0500

    home spacing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,11 @@ import "./Home.css";
 
 function Home() {
     return (
-        <div>
-            <div className="container">
-                <h2>Hello there!</h2>
+        <div className="containerHome">
+            <div>
+                <h2 className="hello">Hello there!</h2>
             </div>
-            <div className="container">
+            <div>
                 <p>I'm Ben and I'm <button className="buttonTeacher">a teacher &#127822;</button> & <button className="buttonWebDev">a web developer &#128421;</button> .</p>
                 <br />
                 <p className="floating">Learn more by clicking above</p>
